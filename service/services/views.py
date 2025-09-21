375c8e8ab962edcf474751b4848b88e82ac3b3fc
DM3 <dmitry004@mail.ru>
2025-09-21 21:55:52 +0300

    finish lesson 4

--- service/services/views.py
+++ service/services/views.py
@@ -1,6 +1,7 @@
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import Subscription
 from .serializers import SubscriptionSerializer
+from django.db.models import F, Sum
 
 
 # Create your views here.
@@ -15,6 +16,22 @@ class SubscriptionView(ReadOnlyModelViewSet):
             "client__user__email",
             "plan__play_type",
             "plan__discount_percent",
+            # "service__full_price",
+        )
+        .annotate(
+            price=F("service__full_price")
+            - F("service__full_price") * F("plan__discount_percent") / 100.00
         )
     )
     serializer_class = SubscriptionSerializer
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        response = super().list(request, *args, **kwargs)
+
+        response_data = {"result": response.data}
+        response_data["total_amount"] = queryset.aggregate(total=Sum("price")).get(
+            "total"
+        )
+        response.data = response_data
+        return response
